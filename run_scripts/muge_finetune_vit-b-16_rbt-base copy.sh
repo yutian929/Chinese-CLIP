71c8a690ce0500701230b30ec321798e31de3ed6
yutian929 <yutian929@outlook.com>
2025-04-21 16:33:29 +0800

    update finetuning scripts.

--- run_scripts/muge_finetune_vit-b-16_rbt-base copy.sh
+++ run_scripts/muge_finetune_vit-b-16_rbt-base copy.sh
@@ -8,11 +8,11 @@
 # Command: bash run_scripts/muge_finetune_vit-b-16_rbt-base.sh ${DATAPATH}
 
 # Number of GPUs per GPU worker
-GPUS_PER_NODE=1 
+GPUS_PER_NODE=8 
 # Number of GPU workers, for single-worker training, please set to 1
 WORKER_CNT=1
 # The ip address of the rank-0 worker, for single-worker training, please set to localhost
-export MASTER_ADDR=localhost
+export MASTER_ADDR=XX.XX.XX.XX
 # The port for communication
 export MASTER_PORT=8514
 # The rank of this worker, should be in {0, ..., WORKER_CNT-1}, for single-worker training, please set to 0
@@ -34,18 +34,18 @@ reset_optimizer="--reset-optimizer"
 
 # output options
 output_base_dir=${DATAPATH}/experiments/
-name=muge_finetune_vit-b-16_roberta-base_bs32_2080ti
+name=muge_finetune_vit-b-16_roberta-base_bs128_8gpu
 save_step_frequency=999999 # disable it
 save_epoch_frequency=1
-log_interval=10
+log_interval=1
 report_training_batch_acc="--report-training-batch-acc"
 # report_training_batch_acc=""
 
 # training hyper-params
 context_length=52
 warmup=100
-batch_size=32
-valid_batch_size=32
+batch_size=128
+valid_batch_size=128
 accum_freq=1
 lr=5e-5
 wd=0.001
@@ -56,35 +56,30 @@ vision_model=ViT-B-16
 text_model=RoBERTa-wwm-ext-base-chinese
 use_augment="--use-augment"
 # use_augment=""
-accum_freq=4
 
-torchrun --nproc_per_node=${GPUS_PER_NODE} \
-         --nnodes=${WORKER_CNT} \
-         --node_rank=${RANK} \
-         --master_addr=${MASTER_ADDR} \
-         --master_port=${MASTER_PORT} \
-         -- cn_clip/training/main.py \
-         --train-data=${train_data} \
-         --val-data=${val_data} \
-         --resume=${resume} \
-         ${reset_data_offset} \
-         ${reset_optimizer} \
-         --logs=${output_base_dir} \
-         --name=${name} \
-         --save-step-frequency=${save_step_frequency} \
-         --save-epoch-frequency=${save_epoch_frequency} \
-         --log-interval=${log_interval} \
-         ${report_training_batch_acc} \
-         --context-length=${context_length} \
-         --warmup=${warmup} \
-         --batch-size=${batch_size} \
-         --valid-batch-size=${valid_batch_size} \
-         --valid-step-interval=${valid_step_interval} \
-         --valid-epoch-interval=${valid_epoch_interval} \
-         --accum-freq=${accum_freq} \
-         --lr=${lr} \
-         --wd=${wd} \
-         --max-epochs=${max_epochs} \
-         --vision-model=${vision_model} \
-         ${use_augment} \
-         --text-model=${text_model}
+python3 -m torch.distributed.launch --use_env --nproc_per_node=${GPUS_PER_NODE} --nnodes=${WORKER_CNT} --node_rank=${RANK} \
+          --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} cn_clip/training/main.py \
+          --train-data=${train_data} \
+          --val-data=${val_data} \
+          --resume=${resume} \
+          ${reset_data_offset} \
+          ${reset_optimizer} \
+          --logs=${output_base_dir} \
+          --name=${name} \
+          --save-step-frequency=${save_step_frequency} \
+          --save-epoch-frequency=${save_epoch_frequency} \
+          --log-interval=${log_interval} \
+          ${report_training_batch_acc} \
+          --context-length=${context_length} \
+          --warmup=${warmup} \
+          --batch-size=${batch_size} \
+          --valid-batch-size=${valid_batch_size} \
+          --valid-step-interval=${valid_step_interval} \
+          --valid-epoch-interval=${valid_epoch_interval} \
+          --accum-freq=${accum_freq} \
+          --lr=${lr} \
+          --wd=${wd} \
+          --max-epochs=${max_epochs} \
+          --vision-model=${vision_model} \
+          ${use_augment} \
+          --text-model=${text_model}
